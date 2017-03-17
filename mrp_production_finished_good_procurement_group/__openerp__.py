05374615f5f1adfa5b3421e6e4c84e3322f9a2fc
Andhitia Rama <andhitia.r@gmail.com>
2017-03-17 11:38:26 +0700

    [ADD][8.0] mrp_production_finished_good_procurement_group

---
+++ mrp_production_finished_good_procurement_group/__openerp__.py
@@ -0,0 +1,18 @@
+# -*- coding: utf-8 -*-
+# Copyright 2016 OpenSynergy Indonesia
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
+{
+    "name": "Create Procurement Group On Manufacturing Order's Finished Good",
+    "version": "8.0.1.0.0",
+    "category": "Manufacturing",
+    "website": "https://opensynergy-indonesia.com/",
+    "author": "OpenSynergy Indonesia",
+    "license": "AGPL-3",
+    "installable": True,
+    "depends": [
+        "mrp_production_raw_material_procurement_group",
+    ],
+    "data": [
+        "views/mrp_production_views.xml",
+    ],
+}
