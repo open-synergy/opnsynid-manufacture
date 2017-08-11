1371733c9bd92dbf6bffb4f28c1eba1af8818f37
Andhitia Rama <andhitia.r@gmail.com>
2017-08-11 20:50:53 +0700

    [8.0.1.1.0][mrp_production_manual_consume_produce]
    * Add cancel button
    * Remove scrap button

--- mrp_production_manual_consume_produce/__openerp__.py
+++ mrp_production_manual_consume_produce/__openerp__.py
@@ -3,7 +3,7 @@
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
 {
     "name": "Manufacturing Order's Manual Control to Consume and Produce",
-    "version": "8.0.1.0.0",
+    "version": "8.0.1.1.0",
     "category": "Manufacturing",
     "website": "https://opensynergy-indonesia.com/",
     "author": "OpenSynergy Indonesia",
