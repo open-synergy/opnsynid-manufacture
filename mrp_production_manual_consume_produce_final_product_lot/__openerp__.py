cfa99e149a76e94744dea205b6775f2bb4ad5726
Andhitia Rama <andhitia.r@gmail.com>
2017-08-11 19:04:49 +0700

    [8.0.1.0.1][mrp_production_manual_consume_produce_final_product_lot
    Auto install True

--- mrp_production_manual_consume_produce_final_product_lot/__openerp__.py
+++ mrp_production_manual_consume_produce_final_product_lot/__openerp__.py
@@ -3,12 +3,13 @@
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
 {
     "name": "MO's Manual Control to Consume and Produce - Final Product Lot",
-    "version": "8.0.1.0.0",
+    "version": "8.0.1.0.1",
     "category": "Manufacturing",
     "website": "https://opensynergy-indonesia.com/",
     "author": "OpenSynergy Indonesia",
     "license": "AGPL-3",
     "installable": True,
+    "auto_install": True,
     "depends": [
         "mrp_production_manual_consume_produce",
         "mrp_final_product_lot",
