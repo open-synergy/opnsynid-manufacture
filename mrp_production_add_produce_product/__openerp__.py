f59070a2dc2edab1a6610192c512d35ffd623f9a
Andhitia Rama <andhitia.r@gmail.com>
2017-08-12 11:31:28 +0700

    [8.0.1.0.1][mrp_production_add_produce_product]
    * Assign group_id when MO created by procurement

--- mrp_production_add_produce_product/__openerp__.py
+++ mrp_production_add_produce_product/__openerp__.py
@@ -3,7 +3,7 @@
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
 {
     "name": "Add Additional Produced Products on MO",
-    "version": "8.0.1.0.0",
+    "version": "8.0.1.0.1",
     "category": "Manufacturing",
     "website": "https://opensynergy-indonesia.com/",
     "author": "OpenSynergy Indonesia",
