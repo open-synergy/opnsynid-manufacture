f2b83ed95f8aac17f2dcec4ac02769028ba1d70b
Andhitia Rama <andhitia.r@gmail.com>
2017-08-12 11:54:29 +0700

    [8.0.1.0.0][mrp_production_produce_product_procurement_group

---
+++ mrp_production_add_produce_product_procurement_group/__openerp__.py
@@ -0,0 +1,19 @@
+# -*- coding: utf-8 -*-
+# Copyright 2017 OpenSynergy Indonesia
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
+{
+    "name": "Procurement Group for Mo's Additional Produced Products",
+    "version": "8.0.1.0.0",
+    "category": "Manufacturing",
+    "website": "https://opensynergy-indonesia.com/",
+    "author": "OpenSynergy Indonesia",
+    "license": "AGPL-3",
+    "installable": True,
+    "auto_install": True,
+    "depends": [
+        "mrp_production_add_produce_product",
+        "mrp_production_finished_good_procurement_group",
+    ],
+    "data": [
+    ],
+}
---
+++ mrp_production_add_produce_product_procurement_group/wizards/mrp_add_produce_product.py
@@ -0,0 +1,20 @@
+# -*- coding: utf-8 -*-
+# Copyright 2017 OpenSynergy Indonesia
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
+
+from openerp import models, api
+
+
+class AddProduceProduct(models.TransientModel):
+    _inherit = "mrp.add_produce_product"
+
+    @api.multi
+    def _get_proc_group_id(self):
+        self.ensure_one()
+        group_id = super(AddProduceProduct, self)._get_proc_group_id()
+        production_id = self.env.context.get("active_id", False)
+        production = self.env["mrp.production"].browse([production_id])[0]
+        if production.fg_procurement_group_id:
+            return production.fg_procurement_group_id.id
+        else:
+            return group_id
