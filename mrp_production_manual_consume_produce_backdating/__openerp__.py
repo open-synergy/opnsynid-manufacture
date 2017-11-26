32e920256af88997bc28a186ad078e9d8ac8d2a1
Andhitia Rama <andhitia.r@gmail.com>
2017-11-26 20:55:48 +0700

    [8.0.1.0.0] mrp_production_manual_consume_produce_backdating

---
+++ mrp_production_manual_consume_produce_backdating/__openerp__.py
@@ -0,0 +1,20 @@
+# -*- coding: utf-8 -*-
+# Copyright 2017 OpenSynergy Indonesia
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
+{
+    "name": "Backdating on Manufacturing Order",
+    "version": "8.0.1.0.0",
+    "category": "Manufacturing",
+    "website": "https://opensynergy-indonesia.com/",
+    "author": "OpenSynergy Indonesia",
+    "license": "AGPL-3",
+    "installable": True,
+    "depends": [
+        "mrp_production_manual_consume_produce",
+        "stock_move_backdating",
+    ],
+    "data": [
+        "wizards/mrp_produce_finished_good_views.xml",
+        "wizards/stock_move_consume_views.xml",
+    ],
+}
