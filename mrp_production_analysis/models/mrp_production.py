79a1cbbe868fa92e6a8593e64d1019c511f3e96b
Andhitia Rama <andhitia.r@gmail.com>
2019-01-16 20:59:32 +0700

    [8.0.1.0.0] mrp_production_analysis

---
+++ mrp_production_analysis/models/mrp_production.py
@@ -0,0 +1,25 @@
+# -*- coding: utf-8 -*-
+# Copyright 2018 OpenSynergy Indonesia
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
+
+from openerp import models, fields
+
+
+class MrpProduction(models.Model):
+    _inherit = "mrp.production"
+
+    rm_plan_summary_ids = fields.One2many(
+        string="RM Plan Summary",
+        comodel_name="mrp.production_rm_plan_summary",
+        inverse_name="production_id",
+    )
+    rm_realization_summary_ids = fields.One2many(
+        string="RM Realization Summary",
+        comodel_name="mrp.production_rm_realization_summary",
+        inverse_name="production_id",
+    )
+    rm_analysis_ids = fields.One2many(
+        string="RM Analysis",
+        comodel_name="mrp.production_rm_analysis",
+        inverse_name="production_id",
+    )
---
+++ mrp_production_analysis/models/mrp_production_rm_realization_summary.py
@@ -0,0 +1,80 @@
+# -*- coding: utf-8 -*-
+# Copyright 2018 OpenSynergy Indonesia
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
+
+from openerp import models, fields
+from openerp import tools
+
+
+class MrpProductionRmRealizationSummary(models.Model):
+    _name = "mrp.production_rm_realization_summary"
+    _description = "Production Order RM Realization Summary"
+    _auto = False
+
+    production_id = fields.Many2one(
+        string="# MO",
+        comodel_name="mrp.production",
+    )
+    product_id = fields.Many2one(
+        string="Product",
+        comodel_name="product.product",
+    )
+    quantity = fields.Float(
+        string="Quantity",
+    )
+
+    def _select(self):
+        select_str = """
+        SELECT
+            row_number() OVER() as id,
+            b.id AS production_id,
+            a.product_id AS product_id,
+            SUM(a.product_qty) AS quantity
+        """
+        return select_str
+
+    def _from(self):
+        from_str = """
+                stock_move AS a
+        """
+        return from_str
+
+    def _where(self):
+        where_str = """
+        WHERE 1 = 1 AND
+            a.state = 'done'
+        """
+        return where_str
+
+    def _join(self):
+        join_str = """
+        JOIN mrp_production AS b ON
+            a.raw_material_production_id = b.id
+        """
+        return join_str
+
+    def _group_by(self):
+        group_str = """
+        GROUP BY
+            b.id,
+            a.product_id
+        """
+        return group_str
+
+    def init(self, cr):
+        tools.drop_view_if_exists(cr, self._table)
+        # pylint: disable=locally-disabled, sql-injection
+        cr.execute("""CREATE or REPLACE VIEW %s as (
+            %s
+            FROM %s
+            %s
+            %s
+            %s
+        )""" % (
+            self._table,
+            self._select(),
+            self._from(),
+            self._join(),
+            self._where(),
+            self._group_by()
+        ))
