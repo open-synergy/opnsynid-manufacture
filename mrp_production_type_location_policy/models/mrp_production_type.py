e464b1d9d4e8744372f043a10569a546e018001c
mikevhe18 <viriyananda.michael@gmail.com>
2019-06-14 09:20:03 +0700

    [8.0.1.0.0][mrp_production_type_location_policy]

---
+++ mrp_production_type_location_policy/models/mrp_production_type.py
@@ -0,0 +1,37 @@
+# -*- coding: utf-8 -*-
+# Copyright 2019 OpenSynergy Indonesia
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
+
+from openerp import models, fields
+
+
+class MrpProductionType(models.Model):
+    _inherit = "mrp.production_type"
+
+    default_raw_material_location_id = fields.Many2one(
+        string="Default Raw Material Location",
+        domain="[('usage', '=', 'internal')]",
+        comodel_name="stock.location"
+    )
+
+    allowed_raw_material_location_ids = fields.Many2many(
+        string="Allowed Raw Materials Location",
+        comodel_name="stock.location",
+        domain="[('usage', '=', 'internal')]",
+        relation="mo_type_raw_location_rel",
+        column1="mo_type_id",
+        column2="location_id")
+
+    default_finished_prod_location_id = fields.Many2one(
+        string="Default Finished Product Location",
+        domain="[('usage', '=', 'internal')]",
+        comodel_name="stock.location"
+    )
+
+    allowed_finished_prod_location_ids = fields.Many2many(
+        string="Allowed Finished Products Location",
+        comodel_name="stock.location",
+        domain="[('usage', '=', 'internal')]",
+        relation="mo_type_finished_location_rel",
+        column1="mo_type_id",
+        column2="location_id")
