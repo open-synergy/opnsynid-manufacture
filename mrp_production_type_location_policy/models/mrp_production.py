d187940770696629038408b2c958e5ff90390dd3
mikevhe18 <viriyananda.michael@gmail.com>
2019-06-14 09:20:03 +0700

    [8.0.1.0.0][mrp_production_type_location_policy]

---
+++ mrp_production_type_location_policy/models/mrp_production.py
@@ -0,0 +1,47 @@
+# -*- coding: utf-8 -*-
+# Copyright 2019 OpenSynergy Indonesia
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
+
+from openerp import models, api, fields
+
+
+class MrpProduction(models.Model):
+    _inherit = "mrp.production"
+
+    @api.onchange("type_id")
+    def onchange_location_src_id(self):
+        self.location_src_id = False
+        if self.type_id:
+            self.location_src_id =\
+                self.type_id.default_raw_material_location_id.id
+
+    @api.onchange("type_id")
+    def onchange_location_dest_id(self):
+        self.location_dest_id = False
+        if self.type_id:
+            self.location_dest_id =\
+                self.type_id.default_finished_prod_location_id.id
+
+    @api.depends("type_id")
+    def _compute_allowed_location(self):
+        for mo in self:
+            self.allowed_location_src_ids = []
+            self.allowed_location_dest_ids = []
+            if mo.type_id:
+                mo.allowed_location_src_ids = \
+                    mo.type_id.allowed_raw_material_location_ids.ids
+                mo.allowed_location_dest_ids = \
+                    mo.type_id.allowed_finished_prod_location_ids.ids
+
+    allowed_location_src_ids = fields.Many2many(
+        string="Allowed Raw Materials Location",
+        comodel_name="stock.location",
+        compute="_compute_allowed_location",
+        store=False,
+    )
+    allowed_location_dest_ids = fields.Many2many(
+        string="Allowed Finished Products Location",
+        comodel_name="stock.location",
+        compute="_compute_allowed_location",
+        store=False,
+    )
