b63f96fab862f3a13a6a68e82aea58649f0b6796
Andhitia Rama <andhitia.r@gmail.com>
2020-01-22 07:26:08 +0700

    [8.0.3.0.1] mrp_production_real_cost_byproduct
    No need to run product_price_update_production_done method when production finished

--- mrp_production_real_cost_byproduct/__openerp__.py
+++ mrp_production_real_cost_byproduct/__openerp__.py
@@ -3,7 +3,7 @@
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
 {
     "name": "Manufacturing Order Byproduct Real Cost",
-    "version": "8.0.3.0.0",
+    "version": "8.0.3.0.1",
     "category": "Manufacturing",
     "website": "https://opensynergy-indonesia.com/",
     "author": "OpenSynergy Indonesia",
